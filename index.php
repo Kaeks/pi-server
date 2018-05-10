a9d68b0353df8edf84f3ff82bc55235a596abf47
Kaeks <Kaeks@users.noreply.github.com>
2018-05-10 23:26:31 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <title>Pi Webserver Project</title>
+</head>
+<body>
+  It works!
+  <?php
+  for ($i = 0; $i < 10; $i++) {
+    echo "$i<br>";
+  }
+  ?>
+</body>
+</html>
